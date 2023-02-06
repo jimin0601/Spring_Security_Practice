7749f952edff214fb1adb180bea05bbedd3be551
jimin0601 <jimin600155@naver.com>
2023-02-06 19:01:27 +0900

    23.02.06 jwt 구ã현 ì¤

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -15,12 +15,21 @@ import java.util.Arrays;
 
 // @Configuration : Bean 등록을 명시함
 @Configuration
+// EnableWebSecurity : 기본적인 Web 보안을 활성화
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable();
+
+        http
+                // authorizeRequests() : HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다는 의미
+                .authorizeRequests()
+                // antMatcher() : /v1/api/hello에 대한 요청은 인증없이 접근을 허용
+                .antMatchers("/v1/api/hello").permitAll()
+                // anyRequest().authenticated() : 나머지 요청들은 모두 인증되어야 함
+                .anyRequest().authenticated();
     }
 
     @Bean
