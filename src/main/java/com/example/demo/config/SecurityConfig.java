f78a01a24d419408691e971505e6003ed5d49490
jimin0601 <jimin600155@naver.com>
2023-02-07 19:27:58 +0900

    02.07 jm JWT security study

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -22,7 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.cors().and().csrf().disable();
-
         http
                 // authorizeRequests() : HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다는 의미
                 .authorizeRequests()
